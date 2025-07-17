15a44c551496f1129c7e064289515a83f352d401
ImmortalMan <ogulcan_topal_2001@hotmail.com>
2025-07-17 04:12:32 +0300

    Add zombie aimbot feature

--- 7d2dMonoInternal/Features/Aimbot/Aimbot.cs
+++ 7d2dMonoInternal/Features/Aimbot/Aimbot.cs
@@ -1,157 +1,54 @@
-﻿//using System;
-//using System.Runtime.InteropServices;
-//using UnityEngine;
-//using Eutl = SevenDTDMono.ESPUtils;
-//using O = SevenDTDMono.Objects;
-//using SETT = SevenDTDMono.NewSettings;
+using UnityEngine;
+using System.Collections.Generic;
 
-//namespace SevenDTDMono
-//{
-//    public class Aimbot : MonoBehaviour
-//    {
-//        [DllImport("user32.dll")]//
-//        private static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+namespace SevenDTDMono.Features
+{
+    public class Aimbot : MonoBehaviour
+    {
+        private static NewSettings SettingsInstance => NewSettings.Instance;
+        private static Dictionary<string, bool> BoolDict => SettingsInstance.GetChildDictionary<bool>(nameof(Dictionaries.BOOL_DICTIONARY));
+        private static EntityPlayerLocal Player => NewSettings.EntityLocalPlayer;
 
-//        private void Start()
-//        {
-//        }
+        private void Update()
+        {
+            if (!SettingsInstance.GetBoolValue(nameof(SettingsBools.AIMBOT)))
+            {
+                return;
+            }
 
-//        private void _Aimbot()
-//        {
-//            // This aimbot is pasted, if you wrote this let me know and I'll credit you.
-//            float minDist = 9999f;
+            if (Player == null || !NewSettings.GameManager.gameStateManager.bGameStarted)
+            {
+                return;
+            }
 
-//            Vector2 target = Vector2.zero;
+            EntityAlive bestZombie = null;
+            float minAngle = float.MaxValue;
+            Vector3 playerHead = Player.emodel.GetHeadTransform().position;
 
-//            foreach (EntityZombie zombie in O.ListZombie)
-//                if (zombie && zombie.IsAlive())
-//                {
-//                    Vector3 lookAt = zombie.emodel.GetBellyPosition();
-//                    Vector3 w2s = ESP.mainCam.WorldToScreenPoint(lookAt);
+            foreach (EntityAlive zombie in NewSettings.EntityAlive)
+            {
+                if (zombie == null || !zombie.IsAlive())
+                {
+                    continue;
+                }
 
-//                    // If they're outside of our FOV.
-//                    if (Vector2.Distance(new Vector2(Screen.width / 2, Screen.height / 2), new Vector2(w2s.x, w2s.y)) > 150f)
-//                        continue;
+                Vector3 head = zombie.emodel.GetHeadTransform().position;
+                Vector3 direction = head - playerHead;
+                float angle = Vector3.Angle(Player.transform.forward, direction);
+                if (angle < minAngle)
+                {
+                    minAngle = angle;
+                    bestZombie = zombie;
+                }
+            }
 
-//                    if (Eutl.IsOnScreen(w2s))
-//                    {
-//                        float distance = Math.Abs(Vector2.Distance(new Vector2(w2s.x, Screen.height - w2s.y), new Vector2(Screen.width / 2, Screen.height / 2)));
-
-//                        if (distance < minDist)
-//                        {
-//                            minDist = distance;
-//                            target = new Vector2(w2s.x, Screen.height - w2s.y);
-//                        }
-//                    }
-//                }
-
-//            if (O.PlayerList.Count > 1)
-//            {
-//                foreach (EntityPlayer player in O.PlayerList)
-//                {
-//                    if (player && player.IsAlive() && player != O.ELP)
-//                    {
-//                        Vector3 lookAt = player.emodel.GetBellyPosition();
-//                        Vector3 w2s = ESP.mainCam.WorldToScreenPoint(lookAt);
-
-//                        if (Vector2.Distance(new Vector2(Screen.width / 2, Screen.height / 2), new Vector2(w2s.x, w2s.y)) > 150f)
-//                            continue;
-
-//                        if (Eutl.IsOnScreen(w2s))
-//                        {
-//                            float distance = Math.Abs(Vector2.Distance(new Vector2(w2s.x, Screen.height - w2s.y), new Vector2(Screen.width / 2, Screen.height / 2)));
-
-//                            if (distance < minDist)
-//                            {
-//                                minDist = distance;
-//                                target = new Vector2(w2s.x, Screen.height - w2s.y);
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-
-//            if (target != Vector2.zero)
-//            {
-//                double distX = target.x - Screen.width / 2f;
-//                double distY = target.y - Screen.height / 2f;
-
-//                distX /= 10;
-//                distY /= 10;
-
-//                mouse_event(0x0001, (int)distX, (int)distY, 0, 0);
-//            }
-//        }
-
-//        private void _MagicBullet()
-//        {
-//            EntityZombie ztarget = null;
-//            EntityPlayer pTarget = null;
-
-//            foreach (EntityZombie zombie in O.ListZombie)
-//                if (zombie && zombie.IsAlive())
-//                {
-//                    Vector3 head = zombie.emodel.GetHeadTransform().position;
-//                    Vector3 w2s = ESP.mainCam.WorldToScreenPoint(head);
-
-//                    // If they're outside of our FOV.
-//                    if (Vector2.Distance(new Vector2(Screen.width / 2, Screen.height / 2), new Vector2(w2s.x, w2s.y)) > 120f)
-//                        continue;
-
-//                    ztarget = zombie;
-//                }
-
-//            foreach (EntityPlayer player in O.PlayerList)
-//            {
-//                if (player && player.IsAlive() && player != O.ELP)
-//                {
-//                    Vector3 head = player.emodel.GetHeadTransform().position;
-//                    Vector3 w2s = ESP.mainCam.WorldToScreenPoint(head);
-
-//                    if (Vector2.Distance(new Vector2(Screen.width / 2, Screen.height / 2), new Vector2(w2s.x, w2s.y)) > 120f)
-//                        continue;
-
-//                    pTarget = player;
-//                }
-//            }
-
-//            if (pTarget)
-//            {
-//                // Purposely not giving the damage source an ID, so servers can't track you for killing people.
-//                DamageSource source = new DamageSource(EnumDamageSource.External, EnumDamageTypes.Concuss);
-
-//                ztarget.DamageEntity(source, 100, false, 1f);
-//                ztarget.AwardKill(O.ELP);
-//            }
-
-//            if (ztarget)
-//            {
-//                DamageSource source = new DamageSource(EnumDamageSource.External, EnumDamageTypes.Concuss);
-//                source.CreatorEntityId = O.ELP.entityId;
-
-//                ztarget.DamageEntity(source, 100, false, 1f);
-//                ztarget.AwardKill(O.ELP);
-
-//                O.ELP.AddKillXP(ztarget);
-//            }
-//        }
-
-//        private void Update()
-//        {
-//            /*if (!Input.anyKey || !Input.anyKeyDown) {
-//                return;
-//            }*/
-
-//            if (Input.GetKey(KeyCode.LeftAlt) && SETT.magicBullet)
-//            {
-//                _MagicBullet();
-//            }
-
-//            if (Input.GetKey(KeyCode.LeftAlt) && O.ListZombie.Count > 0 && SETT.aimbot)
-//            {
-//                _Aimbot();
-//            }
-//        }
-
-//    }
-//}
+            if (bestZombie != null)
+            {
+                Vector3 head = bestZombie.emodel.GetHeadTransform().position;
+                Vector3 direction = head - playerHead;
+                Quaternion look = Quaternion.LookRotation(direction);
+                Player.transform.rotation = Quaternion.Slerp(Player.transform.rotation, look, Time.deltaTime * 15f);
+            }
+        }
+    }
+}
